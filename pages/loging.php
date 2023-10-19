63408da913a530f0d311c1172ae725620a107a8f
Jayani Malshika <99637011+JayaniMalshika@users.noreply.github.com>
2023-10-19 17:10:25 +0530

    update

--- pages/loging.php
+++ pages/loging.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>CodePen - Sign up / Login Form</title>
-  <link rel="stylesheet" href="./style.css">
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Registration</title>
+    <link rel="stylesheet" type="text/css" href="../css/loging.css">
 </head>
 <body>
 <!-- partial:index.partial.html -->
